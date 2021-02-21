209b726ffc45bedd2ed7b17b3d031a1dbc35db86
Linlin <denglinlin315@gmail.com>
2021-02-21 14:21:39 +1000

    Link tickets and users upon ticket creation

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,6 +1,10 @@
 FactoryBot.define do
   factory :ticket do
-    name { "Exmaple ticket" }
-    description { "An example ticket, nothing more" }
+  name { "Example ticket" }
+  description { "An example ticket, nothing more" }
+  association :author, factory: :user
   end
 end
+
+# a_different_user = FactoryBot.create(:user, email: "different@example.com")
+# FactoryBot.create(:ticket, author: a_different_user)
